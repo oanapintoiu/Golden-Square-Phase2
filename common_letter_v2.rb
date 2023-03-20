95f62161e15d5e882ffc3fac5e1c048f34ec16da
oana.pintoiu <oana.pintoiu@yahoo.com>
2023-03-20 16:00:59 +0000

    debugging challenge update v1 and v2

---
+++ common_letter_v2.rb
@@ -0,0 +1,17 @@
+def get_most_common_letter(text)
+    no_space_text = text.gsub(/\s+/, "")
+    counter = Hash.new(0)
+    no_space_text.chars.each do |char|
+      counter[char] += 1
+    # p counter
+    end
+    counter.to_a.sort_by { |k, v| v }[0][0]
+    p counter.to_a.sort_by { |k ,v| v }[-1][0]
+  end
+  
+  puts get_most_common_letter("the roof, the roof, the roof is on fire!")
+
+  # Intended output:
+  # 
+  # > get_most_common_letter("the roof, the roof, the roof is on fire!")
+  # => "o"
