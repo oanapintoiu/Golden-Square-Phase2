bf3616c2321cb5531802ebed2787e5caa7ae56ab
oana.pintoiu <oana.pintoiu@yahoo.com>
2023-03-21 14:49:17 +0000

    single class program gratitudes example

---
+++ single-class-program/spec/gratitudes_spec.rb
@@ -0,0 +1,10 @@
+require "gratitudes"
+
+RSpec.describe Gratitudes do
+it do
+    gratitudes = Gratitudes.new
+    gratitudes.add("your cat")
+    gratitudes.add("nice people")
+    expect(gratitudes.format).to eq "Be grateful for: your cat, nice people"
+end
+end
